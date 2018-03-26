89cc965b175fe148f45d20cf079bd03b663841f9
chellong98 <chellong98.act@gmail.com>
2018-03-26 17:33:19 +0700

    new code

--- adminDashboard/application/views/Info_view.php
+++ adminDashboard/application/views/Info_view.php
@@ -38,7 +38,7 @@
           </a>
         </li>
          <li class="nav-item" datatoggle="tooltip" data-placement="right" title="Tables">
-          <a class="nav-link" href="<?php echo base_url(); ?>/index.php/First_controller/loadInfo">
+          <a class="nav-link" href="#">
             <i class="fa fa-fw fa-table"></i> 
             <span class="nav-link-text">Danh sách thành viên</span>
           </a>
@@ -75,7 +75,7 @@
             </ul>
         </li>
         <li class="nav-item sidenavToggler" style="background-color: #212529">
-          <a id="sidenavToggler" href="#" class="m-auto nav-link text-center">
+          <a id="sidenavToggler" href="#" class="nav-link text-center">
             <i class="fa fa-fw fa-angle-left"></i>
           </a>
         </li>
@@ -111,6 +111,45 @@
               / New feed
             </li>
           </ol>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-10 ml-auto">
+          <div class="card card-block">
+          <div class="card-header">
+            <i class="fa fa-area-chart"></i> Thong tin ca nhan
+          </div>
+          <div class="card-body ">
+            <div class="card-deck card-block">
+              <div class="card ">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM
+          </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ adminDashboard/application/views/Login_view.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>view 1</title>
+	<script type="text/javascript" src="<?php echo base_url(); ?>vendor/bootstrap.js"></script>
+ 	<script type="text/javascript" src="<?php echo base_url(); ?>1.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>vendor/font-awesome/css/font-awesome.min.css">
+	<link href="<?php echo base_url(); ?>vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+ 	<link rel="stylesheet" href="<?php echo base_url(); ?>vendor/1.css">
+
+</head>
+<body class="bg-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 push-sm-2">
+				<div class="card card-login mt-5">
+					<div class="card-header img-circle" align="center" >
+`						<img src="https://bootsnipp.com/img/logo.jpg" width="10%" height="10%" class="img-circle">
+					</div>
+					<div class="card-body">
+						<form action="" method="POST" role="form">
+							<legend>Form title</legend>
+						
+							<div class="form-group">
+								<label for="">label</label>
+								<input type="text" class="form-control" id="" placeholder="Input field">
+							</div>
+						
+							
+						
+							<button type="submit" class="btn btn-primary">Submit</button>
+						</form>
+					</div>
+					<div class="card-footer">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
