74000e9519992e7f1529ba4332674774744b8bd0
chellong98 <chellong98.act@gmail.com>
2018-04-24 12:19:17 +0700

    API user

--- APIUsers/application/models/addData_model.php
+++ APIUsers/application/models/addData_model.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class addData_model extends CI_Model {
-
+  
 	public $variable;
 
 	public function __construct()
@@ -31,6 +31,42 @@ class addData_model extends CI_Model {
 			UserReturn(1, "đăng nhập thất bại",''); //goi helper 
 		}
 		
+	}
+
+	public function postBaiDang()
+	{
+		$idnguoidang = $_POST['idnguoidang']; 
+		$noidung = $_POST['noidung'];
+		$ngayDang = date("Y-m-d H:i:s");
+
+		$baiDang = array(
+			'idnguoidang'=>$idnguoidang,
+			'noiDungBaiDang'=>$noidung,
+			'ngayDang'=>$ngayDang
+		);
+		$this->load->database();
+		$this->db->insert('baidang', $baiDang);
+		// $this->db->select("*");
+		// $this->db->from("usersinfomation");
+		$user = $this->db->get_where('usersinfomation', array('sothutu'=>$idnguoidang));
+		$user = $user->result_array();
+		foreach ($user as $key) {
+			$user = $key;
+		}
+		$response = array(
+			'user'=>$user,
+			'noiDungBaiDang'=>$noidung,
+			'ngayDang'=>$ngayDang
+		);
+
+		
+		// file_put_contents('idnguoidang.txt', $idnguoidang);
+		
+		$this->load->helper('Helper_helper');
+
+		UserReturn(0, "response", $response); 
+
+
 	}
 }
 
